7c17e0be8030b9cb4063ba3b62fd7ff9fe719ba4
Ciarra Murphy <mciarra@ymail.com>
2020-05-17 19:44:22 -0500

    updated files for homework

--- get-post-form.js
+++ get-post-form.js
@@ -11,48 +11,27 @@ app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('port', 60790);
 
+
 app.get('/',function(req,res){
-  res.render('home');
-});
-
-app.get('/show-data',function(req,res){
-  var context = {};
-  context.sentData = req.query.myData;
-  res.render('show-data', context);
-});
-
-app.get('/get-loopback',function(req,res){
-  var qParams = "";
-  for (var p in req.query){
-    qParams += "The name " + p + " contains the value " + req.query[p] + ", ";
-  }
-  qParams = qParams.substring(0,qParams.lastIndexOf(','));
-  qParams += '.';
-  var context = {};
-  context.dataList = qParams;
-  res.render('get-loopback', context);
-});
-
-app.get('/get-loopback-improved',function(req,res){
   var qParams = [];
   for (var p in req.query){
-    qParams.push({'name':p,'value':req.query[p]})
+    qParams.push({'data':p,'result':req.query[p]})
   }
   var context = {};
   context.dataList = qParams;
-  res.render('get-loopback-improved', context);
+  res.render('get-response', context);
 });
 
-app.post('/post-loopback', function(req,res){
+app.post('/', function(req,res){
   var qParams = [];
   for (var p in req.body){
-    qParams.push({'name':p,'value':req.body[p]})
+    qParams.push({'data':p,'result':req.body[p]})
   }
   console.log(qParams);
   console.log(req.body);
   var context = {};
   context.dataList = qParams;
-  res.render('post-loopback', context);
+  res.render('post-response', context);
 });
 
 app.use(function(req,res){
