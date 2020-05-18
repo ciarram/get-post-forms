e18a24a130828ca29f2048b71de00067e17f3711
Ciarra Murphy <mciarra@ymail.com>
2020-05-18 14:38:31 -0500

    finalized code for forms

--- get-post-form.js
+++ get-post-form.js
@@ -4,8 +4,11 @@ var app = express();
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 var bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+/*app.use(bodyParser.urlencoded({ extended: false }));*/
+/*app.use(bodyParser.json());*/
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
@@ -17,22 +20,59 @@ app.get('/',function(req,res){
   for (var p in req.query){
     qParams.push({'data':p,'result':req.query[p]})
   }
-  var context = {};
-  context.dataList = qParams;
-  res.render('get-response', context);
+  var getResults = {};
+  getResults.dataList = qParams;
+  res.render('get-response', getResults);
 });
 
 app.post('/', function(req,res){
-  var qParams = [];
-  for (var p in req.body){
-    qParams.push({'data':p,'result':req.body[p]})
+  var urlParam = [];
+  for (var u in req.query){
+    urlParam.push({'data':u,'result':req.query[u]})
   }
-  console.log(qParams);
+  var jsonParam = [];
+  for (var j in req.body){
+    jsonParam.push({'jdata':j,'jresult':req.body[j]})
+  }
+
+  console.log(urlParam);
+  console.log(req.query);
+  var urlResult = {};
+  urlResult.dataList = urlParam;
+
+  console.log(jsonParam);
   console.log(req.body);
-  var context = {};
-  context.dataList = qParams;
-  res.render('post-response', context);
+  var jsonResult = {};
+  jsonResult.dataList = jsonParam;
+  jsonResult.urlList = urlParam;
+  res.render('url-post-response', jsonResult);
+
+  /*if(Array.isArray(urlParam) && urlParam.length){
+    res.render('url-post-response', context); 
+  }*/ 
+  
+  /*var jsonParam = [];
+  for (var j in req.body){
+    jsonParam.push({'jdata':j, 'jresult':req.body[j]})
+  }
+  console.log(jsonParam);
+  console.log(req.body);
+  var newList = {};
+  newList.dataList = urlParam;
+  res.render('url-post-response', newList);*/
 });
+
+/*app.post('/', function(req,res){
+  var jsonParam = [];
+  for (var j in req.body){
+    jsonParam.push({'jdata':j,'jresult':req.body[j]})
+  }
+  console.log(jsonParam);
+  console.log(req.body);
+  var jsonResult = {};
+  jsonResult.dataList = jsonParam;
+  res.render('json-post-response', jsonResult);
+});*/
 
 app.use(function(req,res){
   res.status(404);
